d06435dd4325430018d10a745faddacc817ce753
Marcelo Diniz <marcelo.diniz.ti@gmail.com>
2023-07-27 14:19:21 -0300

    mdiniz

--- lab_import_existing_resources_into_terraform_from_azure/lab_2_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Lab Setup Script
-mkdir terraformguru
-wget https://raw.githubusercontent.com/ACloudGuru/advanced-terraform-with-azure/main/lab_import_existing_resources_into_terraform_from_azure/providers.tf
-mv providers.tf terraformguru/
-rm -rf lab_2_setup.sh
